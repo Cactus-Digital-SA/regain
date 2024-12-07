d8afb056dcf45c6058d3d8592489f579bd0d13b2
StefanosGiannakos <devintern1@cactusweb.gr>
2024-12-07 22:55:32 +0200

    modal next

--- resources/views/frontend/content/mock/dashboards/organization/includes/new-patient-registration-modal.blade.php
+++ resources/views/frontend/content/mock/dashboards/organization/includes/new-patient-registration-modal.blade.php
@@ -2,45 +2,73 @@
 <div class="modal fade" id="newPatientRegistration" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered" style="max-width: 60%; max-height: 100%">
         <div class="modal-content p-3 p-md-5" style="background-color: rgba(255, 255, 255, 1);">
-            <button type="button" class="btn-close btn-pinned btn-right" data-bs-dismiss="modal" aria-label="Close">
-            </button>
-            <h3 class="text-center mb-4" style="rgba(10, 19, 58, 1); font-weight: 700; font-size: 24px;">New Patient Registration</h3>
-            <form>
-                <div class="row g-4">
-                    <div class="col-md-6">
-                        <label for="input1" class="form-label">Input 1</label>
-                        <input type="text" id="input1" class="form-control" style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+            <div class="modal-body" id="modal-container">
+                <button type="button" class="btn-close btn-pinned btn-right" data-bs-dismiss="modal" aria-label="Close">
+                </button>
+                <h3 class="text-center mb-4" style="rgba(10, 19, 58, 1); font-weight: 700; font-size: 24px;">New Patient
+                    Registration</h3>
+                <form>
+                    <div class="row g-4">
+                        <div class="col-md-6">
+                            <label for="input1" class="form-label">Input 1</label>
+                            <input type="text" id="input1" class="form-control"
+                                   style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="input2" class="form-label">Input 2</label>
+                            <input type="text" id="input2" class="form-control"
+                                   style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="input3" class="form-label">Input 3</label>
+                            <input type="text" id="input3" class="form-control"
+                                   style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="input4" class="form-label">Input 4</label>
+                            <input type="text" id="input4" class="form-control"
+                                   style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="input5" class="form-label">Input 5</label>
+                            <input type="text" id="input5" class="form-control"
+                                   style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="input6" class="form-label">Input 6</label>
+                            <input type="text" id="input6" class="form-control"
+                                   style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="input7" class="form-label">Input 7</label>
+                            <input type="text" id="input7" class="form-control"
+                                   style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
+                        </div>
+                        <div class="col-md-6 mt-5">
+                            <button type="button" class="btn btn-primary btn-next"
+                                    style="background-color: rgba(10, 19, 58, 1); min-width: 250px;" id="btn-next">Next
+                            </button>
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <label for="input2" class="form-label">Input 2</label>
-                        <input type="text" id="input2" class="form-control" style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="input3" class="form-label">Input 3</label>
-                        <input type="text" id="input3" class="form-control" style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="input4" class="form-label">Input 4</label>
-                        <input type="text" id="input4" class="form-control" style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="input5" class="form-label">Input 5</label>
-                        <input type="text" id="input5" class="form-control" style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="input6" class="form-label">Input 6</label>
-                        <input type="text" id="input6" class="form-control" style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="input7" class="form-label">Input 7</label>
-                        <input type="text" id="input7" class="form-control" style="background-color: rgba(221, 222, 241, 1); min-width: 400px;">
-                    </div>
-                    <div class="col-md-6 mt-5">
-                        <button type="button" class="btn btn-primary" style="background-color: rgba(10, 19, 58, 1); min-width: 250px;">Next</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 </div>
 <!--/ Add Role Modal -->
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+
+        let nextButton = document.getElementById('btn-next');
+        // Handle Next button click
+        nextButton.addEventListener('click', function (e) {
+                e.preventDefault();
+                let modal = document.getElementById('modal-container');
+                modal.innerHTML = 'Hello';
+            }
+        );
+
+    })
+    ;
+</script>
