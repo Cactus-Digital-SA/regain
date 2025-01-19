445ecc1c6c820330784e56abe113054b15763af3
StefanosGiannakos <devintern1@cactusweb.gr>
2025-01-19 21:57:12 +0200

    index alt slider mostly done. Updated todo for the last todos on the sliders

--- resources/views/patient/index-alt.blade.php
+++ resources/views/patient/index-alt.blade.php
@@ -9,7 +9,7 @@
     <style>
         .slider{
             margin: auto;
-            width: 90%;
+            width: 100%;
             z-index: 3;
         }
 
@@ -59,7 +59,7 @@
             max-height: 4px;
             position: absolute;
             z-index: 1;
-            max-width: 808px;
+            max-width: 800px;
             width: 100%;
             height: 100%;
         }
@@ -73,7 +73,7 @@
         }
 
         .noUi-pips-horizontal {
-            width: 102%;
+            width: 101%;
         }
 
         .noUi-value.highlighted {
@@ -128,14 +128,35 @@
                     class="collect-question"
                     data-question-id="{{$question->getId()}}"
                     data-condition-question-id="{{$question->getRequiredQuestionId()}}"
-                    data-condition-required-response-ids="[{{implode(", ", $question->getRequiredQuestionResponseIds())}}]">
+                    data-condition-required-response-ids="[{{implode(", ", $question->getRequiredQuestionResponseIds())}}]"
+                    @if ($question->isSelectMultiple())
+                        data-max-selections="{{count($question->getResponses())}}"
+                    @else
+                        data-max-selections="1"
+                    @endif>
                     @csrf
                     <div class="question mb-5">
                         <div class="question-span-div">
                             <span class="question-span">{{$question->getTitle()}} ({{$question->getInstruction()->getContent()}})</span>
                         </div>
-                        <div class="slider-background"></div>
+{{--                        <div class="slider-background"></div>--}}
                         <div class="slider" id="slider-{{ $index }}"></div>
+                        <ul class="list-unstyled mt-3 checkbox-list hidden">
+                            @foreach ($question->getResponses() as $response)
+                                <li>
+                                    <input
+                                        type="checkbox"
+                                        class="select-response"
+                                        data-question-id="{{$question->getId()}}"
+                                        data-response-id="{{$response->getId()}}"
+                                        id="response-{{$question->getId()}}-{{$response->getId()}}"
+                                        name="response-{{$question->getId()}}[]">
+                                    <label for="response-{{$question->getId()}}-{{$response->getId()}}">
+                                        {{$response->getTitle()}}
+                                    </label>
+                                </li>
+                            @endforeach
+                        </ul>
                     </div>
                 </form>
             </div>
@@ -155,98 +176,215 @@
 <script src="https://cdn.jsdelivr.net/npm/nouislider@15.7.0/dist/nouislider.min.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
+        const sliderValues = {}; // Object to track the slider values by questionId
+
+        // Initialize of sliders based on the index of questions
         const questions = @json(array_map(function ($question) {
         return array_map(function ($response) {
-            return $response->getTitle();
-        }, $question->getResponses());
-    }, $presenter->getQuestions()));
-
-        const sliderValues = {}; // Object to track the slider values by questionId
+                return $response->getTitle();
+            }, $question->getResponses());
+        }, $presenter->getQuestions()));
 
         questions.forEach((responses, index) => {
             const sliderElement = document.getElementById(`slider-${index}`);
             const questionId = parseInt(sliderElement.closest('.collect-question').dataset.questionId);
+            const checkboxList = sliderElement.closest('.collect-question').querySelector('.checkbox-list');
+
+            responses = ["", ...responses];
 
             noUiSlider.create(sliderElement, {
                 start: [0],
                 range: {
                     min: 0,
-                    max: responses.length - 1
+                    max: responses.length,
                 },
                 connect: 'lower',
-                behaviour: 'tap-drag',
                 pips: {
                     mode: 'values',
                     values: [...Array(responses.length).keys()],
                     density: 100,
                     format: {
-                        to: value => responses[Math.round(value)]
-                    }
-                }
+                        to: value => responses[Math.round(value)],
+                    },
+                },
             });
 
-            sliderValues[questionId] = 0;
+            sliderValues[questionId] = null;
+            let isSnapping = false; // Prevent multiple snaps during interactions
 
-            let isSnapping = false;
-
-            // Snapping
+            // Snapping functionality
             sliderElement.noUiSlider.on('set', function (values, handle) {
                 if (isSnapping) return;
                 isSnapping = true;
+
                 const snappedValue = Math.round(values[handle]);
                 sliderElement.noUiSlider.set(snappedValue);
-                sliderValues[questionId] = snappedValue; // Update value for question
+                sliderValues[questionId] = snappedValue === 0 ? null : snappedValue - 1; // Adjust for "Select an option"
+
+                // Update checkboxes to reflect snapped value
+                checkboxList.querySelectorAll('input').forEach((checkbox, idx) => {
+                    if (idx === sliderValues[questionId] && idx !== null) {
+                        checkbox.click();
+                    }else{
+                        checkbox.checked = false;
+                        updateNextButtonState();
+                    }
+                });
+
                 isSnapping = false;
             });
 
+            // Highlight slider pips during update
             sliderElement.noUiSlider.on('update', function (values, handle) {
-                const answerIndex = Math.round(values[handle]);
-                document.querySelectorAll(`#slider-${index} .noUi-value`).forEach((pip, i) => {
-                    pip.classList.toggle('highlighted', i === answerIndex);
+                const valueIndex = Math.round(values[handle]);
+                document.querySelectorAll(`#slider-${index} .noUi-value`).forEach((pip, idx) => {
+                    if(idx === 0 || idx === null) {
+                        pip.classList.add('unselected');
+                    }
+                    pip.classList.toggle('highlighted', idx === valueIndex);
+                });
+            });
+
+            // Update slider when checkbox is clicked
+            checkboxList.querySelectorAll('input').forEach((checkbox, idx) => {
+                checkbox.addEventListener('change', function () {
+                    if (checkbox.checked) {
+                        sliderElement.noUiSlider.set(idx + 1); // Adjust for null
+                        sliderValues[questionId] = idx;
+                    }
                 });
             });
         });
 
         const nextButton = document.getElementById('next-button');
-        const overlay = document.getElementById('overlay');
+        const checkboxes = document.querySelectorAll('.select-response');
 
-        nextButton.addEventListener('click', function (e) {
-            e.preventDefault();
+        // Function to handle checkbox change and update the 'Next' button state
+        function updateNextButtonState() {
+            let missing = false;
+            checkboxes.forEach(checkbox => {
+                const questionId = checkbox.dataset.questionId;
+                const form = document.getElementById(`input-form_${questionId}`);
+                const container = form.closest('.container');
+                if (container && container.dataset.hide !== "true") {
+                    const checkboxesForQuestion = form.querySelectorAll('input[name="response-' + questionId + '[]"]:checked');
+                    if (checkboxesForQuestion.length === 0) {
+                        missing = true;
+                    }
+                }
+            });
+            nextButton.disabled = missing;
+        }
 
-            // Generation of json data
-            const selectedResponsesFinal = Object.keys(sliderValues).map(questionId => ({
-                questionId: parseInt(questionId),
-                responses: [sliderValues[questionId]]
-            }));
 
-            // console.log(JSON.stringify({ questions: selectedResponsesFinal }, null, 2));
+        // Event listener for checkboxes
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('click', function (event) {
+                const questionId = this.dataset.questionId;
 
-            // Clear old json data, display new
-            let jsonOutputDiv = document.getElementById('json-output');
-            if (!jsonOutputDiv) {
-                jsonOutputDiv = document.createElement('div');
-                jsonOutputDiv.id = 'json-output';
-                jsonOutputDiv.style.whiteSpace = 'pre-wrap';
-                jsonOutputDiv.style.backgroundColor = '#f8f9fa';
-                jsonOutputDiv.style.padding = '15px';
-                jsonOutputDiv.style.marginTop = '15px';
-                jsonOutputDiv.style.border = '1px solid #ced4da';
-                jsonOutputDiv.style.borderRadius = '5px';
-                nextButton.insertAdjacentElement('afterend', jsonOutputDiv);
-            }
+                const inputForm = document.getElementById(`input-form_` + questionId);
+                const maxSelections = inputForm.dataset.maxSelections;
+                const formAvailableCheckBoxes = inputForm.querySelectorAll('input[name="response-' + questionId + '[]"]');
+                const formCheckedBoxes = Array.from(formAvailableCheckBoxes).filter(checkbox => checkbox.checked);
+                if (formCheckedBoxes.length > maxSelections) {
+                    formAvailableCheckBoxes.forEach(cb => {
+                        if (cb !== event.currentTarget) {
+                            cb.checked = false;
+                        }
+                    });
+                }
 
-            // New json output data
-            jsonOutputDiv.textContent = JSON.stringify({ questions: selectedResponsesFinal }, null, 2);
+                const dependantForms = document.querySelectorAll('form[data-condition-question-id]:not([data-condition-question-id=""])');
+                dependantForms.forEach(form => {
+                    requiredQuestionId = form.dataset.conditionQuestionId;
+                    requiredForm = document.getElementById(`input-form_` + requiredQuestionId);
+                    requiredQuestionResponseIds = JSON.parse(form.dataset.conditionRequiredResponseIds);
+                    parentForm = document.getElementById(`input-form_` + requiredQuestionId);
+                    questionContainer = form.closest('.container');
+                    if (parentForm) {
+                        requiredFormInputs = parentForm.querySelectorAll(`input:checked`);
+                        if (requiredFormInputs.length > 0) {
+                            // Check if any of the selected inputs have a response ID that matches any in the required response IDs array
+                            const containsAnyId = Array.from(requiredFormInputs).some(input =>
+                                requiredQuestionResponseIds.includes(parseInt(input.dataset.responseId))
+                            );
 
-            // Reset the sliders
-            Object.keys(sliderValues).forEach(questionId => {
-                const sliderElement = document.querySelector(
-                    `.collect-question[data-question-id="${questionId}"] .slider`
-                );
-                sliderElement.noUiSlider.set(0);
-                sliderValues[questionId] = 0;
+                            if (containsAnyId) {
+                                if (questionContainer) {
+                                    questionContainer.classList.remove('hidden');
+                                    questionContainer.dataset.hide = "false"
+                                }
+                            } else {
+                                questionContainer.classList.add('hidden');
+                                questionContainer.dataset.hide = "true"
+                            }
+                        } else {
+                            questionContainer.classList.add('hidden');
+                            questionContainer.dataset.hide = "true"
+                        }
+                    }
+
+                });
+                updateNextButtonState();
             });
         });
+
+        // Handle Next button click
+        nextButton.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                // Show the overlay and disable the button
+                overlay.style.display = 'flex';
+                nextButton.disabled = true;
+
+                // Collect the selected response IDs for this question
+                let selectedResponsesFinal = [];
+                document.querySelectorAll(`.collect-question`)
+                    .forEach(questionForm => {
+                        questionContainer = questionForm.closest('.container');
+                        if (questionContainer.dataset.hide === "false") {
+                            questionData = {};
+                            questionData.questionId = parseInt(questionForm.dataset.questionId);
+                            questionData.responses = [];
+
+                            // Add the selected response ID to the questionData object
+                            responses = questionForm.querySelectorAll(`.select-response:checked`).forEach(response => {
+                                questionData.responses.push(parseInt(response.dataset.responseId));
+                            });
+
+                            selectedResponsesFinal.push(questionData);
+                        }
+                    });
+
+                console.log(JSON.stringify({questions: selectedResponsesFinal}));
+                if (selectedResponsesFinal.length > 0) {
+
+                    // Send the collected data via JSON
+                    fetch('{{ route('patient.submit-answers') }}', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        },
+                        body: JSON.stringify({questions: selectedResponsesFinal})
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            // Handle the response from the server (e.g., show a success message, redirect)
+                            window.location.href = '{{ route('patient.home') }}';
+                        })
+                        .catch(error => {
+                            console.error('Error:', error);
+                        });
+                }
+            }
+        );
+
+        overlay.style.display = 'none';
+        nextButton.disabled = false;
+
+        // Initialize the "Next" button state
+        updateNextButtonState();
     });
 </script>
 
