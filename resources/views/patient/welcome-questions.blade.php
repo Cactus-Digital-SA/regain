e1bc41275f3d253633a98a5cb35cb1897dfacd26
StefanosGiannakos <giannakos@cactusweb.gr>
2025-02-08 18:38:27 +0200

    welcome before questions page

---
+++ resources/views/patient/welcome-questions.blade.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Regain</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/nouislider@15.7.0/dist/nouislider.min.css" rel="stylesheet">
+    <style>
+
+        body {
+            overflow-y: hidden;
+        }
+
+        .welcome-back-title {
+            font-weight: 600;
+            font-size: 23px;
+            line-height: 32px;
+            letter-spacing: 0;
+            text-align: center;
+        }
+
+        .dob-container-questions {
+            max-width: 900px !important;
+            width: 100%;
+            text-align: center;
+        }
+
+        .container-inside {
+            text-align: left;
+        }
+
+        .list-unstyled li {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            margin-bottom: 15px;
+        }
+
+        .list-unstyled li::before {
+            content: "⬢"; /* Icon placeholder */
+            font-size: 18px;
+            color: black;
+        }
+
+        .btn-next {
+            background-color: black;
+            border-radius: 20px;
+            padding: 10px 30px;
+            font-size: 16px;
+            min-width: 320px !important;
+            width: 100%;
+        }
+
+        @media screen and (max-width: 1600px) {
+            body {
+                overflow-y: auto;
+            }
+        }
+    </style>
+</head>
+<body>
+
+@vite(['resources/css/patient-index.css'])
+
+@include('patient.includes.navbar')
+
+<div class="dob-container-questions">
+    <div class="container px-5 py-4">
+        <h3 class="text-center pt-0 my-0 welcome-back-title">Welcome to Regain!</h3>
+        <div class="container-inside m-5 text-start d-flex justify-content-center">
+            <ul class="list-unstyled d-flex justify-content-center">
+                <div class="d-flex justify-content-center flex-column gap-3">
+                    <div class="list-item">
+                        <div class="d-flex flex-column">
+                            <div class="row">
+                                <div class="col-1">
+                                    <img src="{{ Vite::asset('resources/images/home-icon-questions/home.svg') }}" alt="">
+                                </div>
+                                <div class="col-11 d-flex flex-column">
+                                    <strong class="list-item-title">Regain</strong>
+                                    <span class="list-item-text">Information about Regain</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="d-flex flex-column">
+                            <div class="row">
+                                <div class="col-1">
+                                    <img src="{{ Vite::asset('resources/images/home-icon-questions/home.svg') }}" alt="">
+                                </div>
+                                <div class="col-11 d-flex flex-column">
+                                    <strong class="list-item-title">My Regain</strong>
+                                    <span class="list-item-text">Manage your profile, progress, and appointments</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="d-flex flex-column">
+                            <div class="row">
+                                <div class="col-1">
+                                    <img src="{{ Vite::asset('resources/images/home-icon-questions/home.svg') }}" alt="">
+                                </div>
+                                <div class="col-11 d-flex flex-column">
+                                    <strong class="list-item-title">Settings</strong>
+                                    <span class="list-item-text">Accessibility options</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="d-flex flex-column">
+                            <div class="row">
+                                <div class="col-1">
+                                    <img src="{{ Vite::asset('resources/images/home-icon-questions/home.svg') }}" alt="">
+                                </div>
+                                <div class="col-11 d-flex flex-column">
+                                    <strong class="list-item-title">Help</strong>
+                                    <span class="list-item-text">Change language, FAQ, updates, report issues</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="d-flex flex-column">
+                            <div class="row">
+                                <div class="col-1">
+                                    <img src="{{ Vite::asset('resources/images/home-icon-questions/home.svg') }}" alt="">
+                                </div>
+                                <div class="col-11 d-flex flex-column">
+                                    <strong class="list-item-title">Live Chat</strong>
+                                    <span class="list-item-text">Say hello to reBot - always online for you!</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ul>
+        </div>
+        <div class="d-flex justify-content-center">
+            <button type="button" class="btn btn-primary btn-next">Next</button>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
